7dbfbe1f6d4d9af8c87c4a297c6b7f7e0b54d11c
lukebro82 <lucas.82@hotmail.com.ar>
2023-11-30 11:06:51 -0300

    nada

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ function main() {
   burgerDiv.addEventListener("click", () => {
     burgerDiv.style.display = "";
   });
+
+  window.addEventListener("resize", function () {
+    if (window.innerWidth >= 500) {
+      const mainSlider1 = document.querySelector(".slider1");
+      const mainSlider2 = document.querySelector(".slider2");
+      const mainSlider3 = document.querySelector(".slider3");
+      const mainSlider4 = document.querySelector(".slider4");
+
+      mainSlider1.style.display = "none";
+    }
+  });
 }
 
 main();
